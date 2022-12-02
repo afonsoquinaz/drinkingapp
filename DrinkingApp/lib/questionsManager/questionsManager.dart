2b5839729129c9af843a6b08826c89bac55a3911
Afonso Quinaz <afonso.quinaz@gmail.com>
2022-12-02 21:20:02 +0000

    alll questions already have answer options that go to feed manager
    alll questions already have answer options that go to feed manager

--- DrinkingApp/lib/questionsManager/questionsManager.dart
+++ DrinkingApp/lib/questionsManager/questionsManager.dart
@@ -63,9 +63,11 @@ class QuestionsManager {
     var doubleValue = Random().nextDouble();
     if (doubleValue <= 0.1) {
       return getWheelOfNames(players);
-    } else if (doubleValue <= 0.45) {
+    } else if (doubleValue <= 0.11) {
+      //This question is the random words question
+      // does not do basically nothing so I reduced the odd a lot
       return getNewQuestion(players);
-    } else if (doubleValue <= 0.8) {
+    } else if (doubleValue <= 0.6) {
       return getMostLikelyTo(players);
     }
     return get1vs1(players);
