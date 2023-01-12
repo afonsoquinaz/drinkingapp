7c6d00ad6620924fcd6880aeeeabadd856381b20
Afonso Quinaz <afonso.quinaz@gmail.com>
2023-01-12 17:26:19 +0000

    Glasses randomize done

--- DrinkingApp/lib/Question.dart
+++ DrinkingApp/lib/Question.dart
@@ -3,9 +3,11 @@ import 'package:flutter/material.dart';
 class Question {
   String type;
   Widget widget;
+  int nbrGlasses;
 
   Question({
     required this.type,
     required this.widget,
+    required this.nbrGlasses,
   });
 }
